992bd632bc130f4122502058dc43901c27f4cae1
Ashish Devade <adevade@optiproerp.com>
2018-07-19 14:39:48 +0530

    changes regarding wono,. added by ashish

--- src/app/move-order/move-order.component.ts
+++ src/app/move-order/move-order.component.ts
@@ -51,29 +51,31 @@ export class MoveOrderComponent implements OnInit {
     element.className = "";
     element.classList.add("opti_body-move-order");
     element.classList.add("opti_account-module");
-   
-
+    //get company name from the session
+    this.CompanyDBId = sessionStorage.getItem('selectedComp');
+    //On Form Initialization get All WO
+    this.getAllWorkOrders();
   }
 
   //This will get all WO
   onWOPress(){
-    this.CompanyDBId = sessionStorage.getItem('selectedComp');
-    this.mo.getAllWorkOrders(this.CompanyDBId).subscribe(
-      data=> {
-       this.allWODetails = data;
-       if(this.allWODetails.length > 0){
-          this.psWONO = this.allWODetails[16].U_O_ORDRNO
-          this.psProductCode = this.allWODetails[16].U_O_PRODID
-          this.psProductDesc = this.allWODetails[16].ItemName
-          this.docEntry = this.allWODetails[16].DocEntry
-          this.psItemManagedBy = this.allWODetails[16].ManagedBy
-       }
-      }
-    )
+    //On Form Initialization get All WO
+    this.getAllWorkOrders();
+    // this.mo.getAllWorkOrders(this.CompanyDBId).subscribe(
+    //   data=> {
+    //    this.allWODetails = data;
+    //    if(this.allWODetails.length > 0){
+    //       this.psWONO = this.allWODetails[16].U_O_ORDRNO
+    //       this.psProductCode = this.allWODetails[16].U_O_PRODID
+    //       this.psProductDesc = this.allWODetails[16].ItemName
+    //       this.docEntry = this.allWODetails[16].DocEntry
+    //       this.psItemManagedBy = this.allWODetails[16].ManagedBy
+    //    }
+    //   }
+    // )
   }
 
   onOperationPress(){
-    this.CompanyDBId = sessionStorage.getItem('selectedComp');
     if(this.psWONO.length > 0){
       this.mo.getOperationByWorkOrder(this.CompanyDBId,this.docEntry,this.psWONO).subscribe(
         data=> {
@@ -84,6 +86,24 @@ export class MoveOrderComponent implements OnInit {
         }
       )
     }
+    else{
+      alert("Select workorder no. first");
+    }
+  }
+
+  //This function will check, if the user entered WO is in the array
+  onWorkOrderChange(){
+   
+    if(this.allWODetails != null && this.allWODetails.length > 0){
+      //to check in te array
+      // for(let rowCount in this.allWODetails){
+      //     if(this.psWONO )
+      // }
+      // let isExists = this.allWODetails.some(function(el){ return el.U_O_ORDRNO === 
+      //   this.psWONO});
+      // console.log('+++--->' + isExists);
+    }
+    
   }
 
   isWorkOrderRightSection:boolean = false;
@@ -151,7 +171,20 @@ export class MoveOrderComponent implements OnInit {
     return data.filter(e => e.U_O_OPERNO == operNo && e.DocEntry == docEntry)
   }
 
-  
+  getAllWorkOrders(){
+    this.mo.getAllWorkOrders(this.CompanyDBId).subscribe(
+      data=> {
+       this.allWODetails = data;
+       if(this.allWODetails.length > 0){
+          // this.psWONO = this.allWODetails[16].U_O_ORDRNO
+          // this.psProductCode = this.allWODetails[16].U_O_PRODID
+          // this.psProductDesc = this.allWODetails[16].ItemName
+          // this.docEntry = this.allWODetails[16].DocEntry
+          // this.psItemManagedBy = this.allWODetails[16].ManagedBy
+       }
+      }
+    )
+  }
 
   // show and hide right content section
   @ViewChild('optirightfixedsection') optirightfixedsection;
