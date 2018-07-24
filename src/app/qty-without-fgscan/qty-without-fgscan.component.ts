e5f73941374a4619667c4cc64eb0fa5e37ebaff7
Ashish Devade <adevade@optiproerp.com>
2018-07-24 14:29:18 +0530

    changws for qtynoscan screen by ashish

--- src/app/qty-without-fgscan/qty-without-fgscan.component.ts
+++ src/app/qty-without-fgscan/qty-without-fgscan.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-qty-without-fgscan',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qty-without-fgscan.component.scss']
 })
 export class QtyWithoutFGScanComponent implements OnInit {
+@Input() basicDetailsFrmMO: any;
 iBalQty:number =0;
 iAcceptedQty:number =0;
 iRejectedQty:number =0;
@@ -13,6 +14,7 @@ iNCQty:number =0;
   constructor() { }
   
   ngOnInit() {
+    this.iBalQty = this.basicDetailsFrmMO[0].BalQty;
   }
 
 }
