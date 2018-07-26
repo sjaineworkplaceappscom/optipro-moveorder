41247f056103a4a0e796acff25d5db10cc21a1a0
Ashish Devade <adevade@optiproerp.com>
2018-07-26 16:14:39 +0530

    changes and fixes for option 2 and 3 screen by ashish

---
+++ src/app/services/fgrmscanparent.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FgrmscanparentService {
+  arrConfigData:any;
+  constructor(private httpclient:HttpClient) { 
+    this.arrConfigData=JSON.parse(localStorage.getItem('arrConfigData'));
+  }
+
+  //defining properties for the call 
+  httpOptions = {
+    headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Accept':'application/json'
+      })
+    };
+
+  //Delete Data for parent FG and child RM
+  public deleteParentFGandRM(CompanyDBID:string,seqNo:number,WONo:string,ParentFGBatchSerNo:string):Observable<any>{
+    //JSON Obeject Prepared to be send as a param to API
+    let jObject:any={ MoveOrder: JSON.stringify([{ CompanyDBID: CompanyDBID,Sequence:seqNo,WorkOrder:WONo,ParentBatchSerial: ParentFGBatchSerNo}]) };
+    //Return the response form the API  
+    return this.httpclient.post(this.arrConfigData.optiProMoveOrderAPIURL+"/MoveOrder/DeleteFGandRM",jObject,this.httpOptions);
+    } 
+}
